f4804084ae8f4ac850136b0590cb4628b4b8a989
CHETAN GANESH <cvganesh4010@gmail.com>
2019-12-24 16:16:52 +0000

    Next change 1

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -11,8 +11,8 @@ sap.ui.define([
 		oMap : null,
 		onAfterRendering:function(){
 			if(!this.oMap){
-				debugger;
-				this.oMap = new google.maps.Map( $("#chetan"),{
+				
+				this.oMap = new 	google.maps.Map( $("#chetan"),{
 					zoom: 12,
 					center:{ lat:28.7041, lng:77.1025 }
 				});
